6a6e0448cd986da7a5e2dca0061ed20b0c42ab7b
dedee42004 <120428440+dedee42004@users.noreply.github.com>
2023-05-28 23:03:09 +0300

    Update example.cpp

--- OOPLab8T/example.cpp
+++ OOPLab8T/example.cpp
@@ -1,173 +1,364 @@
 #include "example.h"
-#include <string.h>
+#include <string>
 #include <iostream>
+using namespace std;
 namespace Fun {
-    // Написати функцію - шаблон послідовного пошуку в максимального серед 4 елементів.
-// Написати специфікацію функції - шаблон для типу char* .
-// Розробити програму тестування шаблона для декількох типів 
-// та специфікації доданого шаблона.   
-////
-
-
- //using namespace std;
-    using std::cout;
-    using std::endl;
-    using std::ostream;
-    using std::istream;
-
-    class TLong {    //  long long 8 -byte
-        long long hi, lo;  // 16 byte 
-    public:
-        TLong() :hi(0), lo(0) {}
-        TLong(long long h, long long l) :hi(h), lo(l) {}
-        TLong& operator = (const TLong& s) {
-            hi = s.hi; lo = s.lo; return *this;
-        };
-        bool      operator > (const TLong& s) {
-            if (hi > s.hi) return true;
-            if (hi == s.hi && lo >= s.lo) return true;
-            return false;
-        }
-        friend ostream& operator<<(ostream& os,
-            TLong& s);
-    };
-
-    ostream& operator<<(ostream& os, TLong& s) {
-        os << "H " << s.hi << ":L " << s.lo << "  ";
-        return os;
-    }
-    //  функція - шаблон
+    //  ГґГіГ­ГЄГ¶ВіГї - ГёГ ГЎГ«Г®Г­
     template <typename T>
-    T max(T x, T y, T z, T u) {
-        T t = x > y ? x : y;
-        T r = z > u ? z : u;
-        return t > r ? t : r;
-    }
-    // специфікація функції - шаблон для типу char*
-    template<>  char* max(char* x, char* y, char* z, char* u)
-    {
-        char* t = strcmp(x, y) ? x : y;
-        char* r = strcmp(z, u) ? z : u;
-        return  strcmp(t, r) ? t : r;
+    void Search(T A[], int n) {
+        T max = A[0], min = A[0];
+        int mi = 0, ma = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (A[i] > max) { max = A[i]; ma = i; }
+            if (A[i] < min) { min = A[i]; mi = i; }
+        }
+        A[ma] = min;
+        A[mi] = max;
+        for (int i = 0; i < n; i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
     }
 
+    //Г±ГЇГҐГ¶ГЁГґВіГЄГ Г¶ВіГї ГґГіГ­ГЄГ¶ВіВї - ГёГ ГЎГ«Г®Г­ Г¤Г«Гї ГІГЁГЇГі char*
+    template<>  void Search(char* A[], int n)
+    {
+        char* max = A[0]; char* min = A[0];
+        int mi = 0, ma = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (strcmp(max, A[i]) == 1) { max = A[i]; ma = i; }
+            if (strcmp(A[i], min) == 1) { min = A[i]; mi = i; }
+        }
+        A[ma] = min;
+        A[mi] = max;
+        for (int i = 0; i < n; i++)
+        {
+            cout << A[i] << " ";
+        }
+        cout << endl;
+    }
 
     int fmain()
     {
         cout << "template\n";
-        int  a = 5, b = 4, k;
-        double r, t = 3.5, q = 3.05;
-        char c, s = 'a', n = 'f';
-        TLong l, i(100, 50), j(50, 100), e(75, 75), f(4, 200);
-        k = max(a, 13, b, 5);   cout << "k= " << k << endl;
-        r = max(t, q, 3.45, 9.32); cout << "r= " << r << endl;
-        c = max(s, 'r', n, 'z');  cout << "c= " << c << endl;
-        l = max(i, j, e, f);    cout << "l= " << l << endl;
-        char* s1 = (char*)"text", * s2 = (char*)"txt", * s3 = (char*)"pltext",
-            * s4 = (char*)"room", * s5;
-        s5 = max<char*>(s1, s2, s3, s4); cout << " s5 =  " << s5 << endl;
+        int  a[10] = { 1,7,2,-1,0,11,7,2,8,0 };
+        double t[10] = { 2,8,9.2,0.2,6.3,8.76,8.7,12,12.8,10 };
+        char  s[10] = { 'a','u','c','p','s','b','z','s','s','t' };
+        cout << "Int: " << endl;
+        Search(a, 10);
+        cout << "Double: " << endl;
+        Search(t, 10);
+        cout << "Char: " << endl;
+        Search(s, 10);
+        cout << "Char* " << endl;
+        char* b[5] = { (char*)"zext",(char*)"bogaboga",(char*)"aabo",(char*)"texta",(char*)"sasha" };
+        Search(b, 5);
         return 0;
     }
-
-
 }
-namespace Stk {
-
-    using namespace std;
-    class TLong {    //  long long 8 -byte
-        long long hi, lo;  // 16 byte 
-    public:
-        TLong() :hi(0), lo(0) {}
-        TLong(long long h, long long l) :hi(h), lo(l) {}
-        TLong& operator = (const TLong& s) {
-            hi = s.hi; lo = s.lo; return *this;
-        };
-        bool      operator > (const TLong& s) {
-            if (hi > s.hi) return true;
-            if (hi == s.hi && lo >= s.lo) return true;
-            return false;
+namespace Fun1 {
+        //  ГґГіГ­ГЄГ¶ВіГї - ГёГ ГЎГ«Г®Г­
+        template <typename T>
+        void Sort(T arr[], int n)
+        {
+            T key;
+            for (int i = 1; i < n; i++)
+            {
+                key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
         }
-        friend ostream& operator<<(ostream& os,
-            TLong& s);
-    };
-    ostream& operator<<(ostream& os, TLong& s) {
-        os << "H " << s.hi << ":L " << s.lo << "  ";
-        return os;
-    }
-    //  stack - шаблон
 
-
-    template <typename T = double, int SIZE = 100>   // <class T>
-    class stack_n
+        //Г±ГЇГҐГ¶ГЁГґВіГЄГ Г¶ВіГї ГґГіГ­ГЄГ¶ВіВї - ГёГ ГЎГ«Г®Г­ Г¤Г«Гї ГІГЁГЇГі char*
+    template<>  void Sort(char* arr[], int n)
     {
-        T m_stk[SIZE];   // Містить елементи стека.
-        int tos;        // Індекс вершини стека.
-    public:
-        stack_n() { tos = 0; }    // Ініціалізує стек.
-        void push(T obj);        // Заштовхує об'єкт у стек.
-        T pop() {        // Виштовхує об'єкт зі стека.
-            if (tos == 0) { cout << "Стек порожній.\n"; return 0; }
-            tos--; return m_stk[tos];
-        }
-    };
-    // Функція що визначається за межами класу
-    template <typename T, int SIZE>
-    void stack_n<T, SIZE>::push(T obj) {
-        if (tos == SIZE) { cout << "Стек повний. \n"; return; }
-        m_stk[tos] = obj; tos++;
+         char* key;
+         for (int i = 1; i < n; i++) 
+         {
+                key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && strcmp(arr[j], key) == 1)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+         }
+         for (int i = 0; i < n; i++)
+         {
+            cout << arr[i] << " ";
+         }
+         cout << endl;
     }
 
-    typename char* pchar;
-    template <int SIZE>   // <class T>
-    class stack_n<char*, SIZE>
-    {
-        char* m_stk[SIZE];   // Містить елементи стека.
-        int tos;        // Індекс вершини стека.
-    public:
-        stack_n() { tos = 0; }    // Ініціалізує стек.
-        void push(char* obj);        // Заштовхує об'єкт у стек.
-        char* pop() {        // Виштовхує об'єкт зі стека.
-            if (tos == 0) { cout << "Стек порожній.\n"; return 0; }
-            tos--; return m_stk[tos];
-        }
-    };
-    // Функція що визначається за межами класу
-    template <int SIZE>
-    void stack_n<char*, SIZE>::push(char* obj) {
-        if (tos == SIZE) { cout << "Стек повний. \n"; return; }
-        m_stk[tos] = obj; tos++;
-    }
     int smain()
     {
-        int i;
-        setlocale(LC_CTYPE, "ukr");
-        std::cout << "Стек тести !\n";
-        stack_n<char, 10> s1, s2;
-        s1.push('a');  s2.push('x');  s1.push('b');
-        s2.push('y');  s1.push('c');  s2.push('z');
-        for (i = 0; i < 3; i++) cout << "Виштовхуємо s1: " << s1.pop() << "\n";
-        for (i = 0; i < 3; i++) cout << "Виштовхуємо s2: " << s2.pop() << "\n";
-        // Демонстрація стека дійсних чисел
-        stack_n<double> ds1, ds2; // Створюємо дві стеки дійсних чисел.
-        ds1.push(1.1);  ds2.push(2.2); ds1.push(3.3);
-        ds2.push(4.4);  ds1.push(5.5); ds2.push(6.6);
-        for (i = 0; i < 3; i++) cout << "Виштовхуємо ds1: " << ds1.pop() << "\n";
-        for (i = 0; i < 3; i++) cout << "Виштовхуємо ds2: " << ds2.pop() << "\n";
-        stack_n <TLong, 10> ln1, ln2;
-        ln1.push(TLong(5, 4));   ln1.push(TLong(50000, 4));   ln1.push(TLong(5, 400000));
-        ln2.push(TLong(3673865, 643287));   ln2.push(TLong(56748, 64374));   ln2.push(TLong(36287, 40530));
-        char* st1 = (char*)"text", * st2 = (char*)"txt", * st3 = (char*)"pltext",
-            * st4 = (char*)"room", * st5 = (char*)"text2";
-
-        stack_n <char*, 5> ss;
-        ss.push(st3); ss.push(st4); ss.push(st5); ss.push(st1);
-        ss.push(st2);
-        for (i = 0; i < 5; i++) cout << "Виштовхуємо ss: " << ss.pop() << "\n";
-        return 0;
+		 cout << endl;
+         cout << "Sorted template\n";
+         int  a[10] = { 1,7,2,-1,0,11,7,2,8,0 };
+         double t[10] = { 2,8,9.2,0.2,6.3,8.76,8.7,12,12.8,10 };
+         char  s[10] = { 'a','u','c','p','s','b','z','s','s','t' };
+         cout << "Int: " << endl;
+         Sort(a, 10);
+         cout << "Double: " << endl;
+         Sort(t, 10);
+         cout << "Char: " << endl;
+         Sort(s, 10);
+         cout << "Char* " << endl;
+         char* b[5] = { (char*)"zext",(char*)"bogaboga",(char*)"aabo",(char*)"texta",(char*)"sasha" };
+         Sort(b, 5);
+         return 0;
     }
 }
+namespace Spus
+{
+	template<typename T>
+	class List
+	{
+	public:
+		List();
+		~List();
+		void pop_front();
+		void push_back(T data);
+		void clear();
+		int GetSize() { return Size; }
+		T& operator[](const int index);
+		void push_front(T data);
+		void insert(T data, int index);
+		void removeAt(int index);
+		void pop_back();
+	private:
+
+		template<typename T>
+		class Node
+		{
+		public:
+			Node* pNext;
+			T data;
+			Node(T data = T(), Node* pNext = nullptr)
+			{
+				this->data = data;
+				this->pNext = pNext;
+			}
+		};
+		int Size;
+		Node<T>* head;
+	};
+
+	template<typename T>
+	List<T>::List()
+	{
+		Size = 0;
+		head = nullptr;
+	}
+
+	template<typename T>
+	List<T>::~List()
+	{
+		clear();
+	}
+
+	template<typename T>
+	void List<T>::push_front(T data)
+	{
+		head = new Node<T>(data, head);
+		Size++;
+	}
+
+	template<typename T>
+	void List<T>::push_back(T data)
+	{
+		if (head == nullptr)
+		{
+			head = new Node<T>(data);
+		}
+		else
+		{
+			Node<T>* current = this->head;
+			while (current->pNext != nullptr)
+			{
+				current = current->pNext;
+			}
+			current->pNext = new Node<T>(data);
+		}
+		Size++;
+	}
+
+	template<typename T>
+	void List<T>::clear()
+	{
+		while (Size)
+		{
+			pop_front();
+		}
+	}
+
+	template<typename T>
+	T& List<T>::operator[](const int index)
+	{
+		int counter = 0;
+		Node<T>* current = this->head;
+		while (current != nullptr)
+		{
+			if (counter == index)
+			{
+				return current->data;
+			}
+			current = current->pNext;
+			counter++;
+		}
+	}
+
+	template<typename T>
+	void List<T>::insert(T data, int index)
+	{
+
+		if (index == 0)
+		{
+			push_front(data);
+		}
+		else
+		{
+			Node<T>* previous = this->head;
+
+			for (int i = 0; i < index - 1; i++)
+			{
+				previous = previous->pNext;
+			}
+			Node<T>* newNode = new Node<T>(data, previous->pNext);
+			previous->pNext = newNode;
+			Size++;
+		}
+	}
+
+	template<typename T>
+	void List<T>::removeAt(int index)
+	{
+		if (index == 0)
+		{
+			pop_front();
+		}
+		else
+		{
+			Node<T>* previous = this->head;
+			for (int i = 0; i < index - 1; i++)
+			{
+				previous = previous->pNext;
+			}
+			Node<T>* toDelete = previous->pNext;
+			previous->pNext = toDelete->pNext;
+			delete toDelete;
+			Size--;
+		}
+	}
+
+	template<typename T>
+	void List<T>::pop_front()
+	{
+		Node<T>* temp = head;
+		head = head->pNext;
+		delete temp;
+		Size--;
+
+	}
+
+	template<typename T>
+	void List<T>::pop_back()
+	{
+		removeAt(Size - 1);
+	}
+	int gmain()
+	{
+		setlocale(LC_ALL, "ukr");
+		cout << endl << "Г‘ГЇГЁГ±Г®ГЄ: " << endl;
+		List<int> lst;
+		lst.push_front(5);
+		lst.push_front(7);
+		lst.push_front(101);
+		lst.push_front(1);
+		lst.push_front(15);
+		lst.push_front(11);
+		lst.push_front(10);
+		lst.push_front(151);
+		for (int i = 0; i < lst.GetSize(); i++)
+		{
+			cout << lst[i] << " ";
+		}
+		cout << endl << "pop_back + pop_front " << endl << endl;
+		lst.pop_back();
+		lst.pop_front();
+		for (int i = 0; i < lst.GetSize(); i++)
+		{
+			cout << lst[i] << " ";
+		}
+		cout << endl << "remove: " << endl << endl;
+		lst.removeAt(2);
+		for (int i = 0; i < lst.GetSize(); i++)
+		{
+			cout << lst[i] <<" ";
+		}
+		cout << endl << "insert: " << endl << endl;
+		lst.insert(65, 1);
+		for (int i = 0; i < lst.GetSize(); i++)
+		{
+			cout << lst[i] << " ";
+		}
+		lst.clear();
+		cout << endl<<endl << "String List:" << endl;
+		List<string> lst1;
+		lst1.push_front("apple");
+		lst1.push_front("car");
+		lst1.push_front("banana");
+		lst1.push_front("people");
+		lst1.push_front("earth");
+		lst1.push_front("hostel");
+		lst1.push_front("nana");
+		lst1.push_front("uni");
+		for (int i = 0; i < lst1.GetSize(); i++)
+		{
+			cout << lst1[i] << " ";
+		}
+		cout << endl << "pop_back + pop_front " << endl << endl;
+		lst1.pop_back();
+		lst1.pop_front();
+		for (int i = 0; i < lst1.GetSize(); i++)
+		{
+			cout << lst1[i] << " ";
+		}
+		cout << endl << "remove: " << endl << endl;
+		lst1.removeAt(2);
+		for (int i = 0; i < lst1.GetSize(); i++)
+		{
+			cout << lst1[i] << " ";
+		}
+		cout << endl << "insert: " << endl << endl;
+		lst1.insert("bonjur", 1);
+		for (int i = 0; i < lst1.GetSize(); i++)
+		{
+			cout << lst1[i] << " ";
+		}
+		lst1.clear();
+		return 0;
+	}
+}
+
 void example()
 {
     Fun::fmain();
-    Stk::smain();
+    Fun1::smain();
+	Spus::gmain();
 }
